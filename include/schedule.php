49801b25d430381ec5be3184170920020a0a3d46
kevinatoranator <kevguy.vet@gmail.com>
2024-07-02 17:21:50 -0400

    Live info and fixes
    Live info on schedule page for Football, Volleyball, Field Hockey
    Fix baseUrl on other sports on schedule

--- include/schedule.php
+++ include/schedule.php
@@ -14,7 +14,7 @@
 			if($sport == "gsoccer" or $sport == "jvgsoccer" or $sport == "bsoccer" or $sport == "jvbsoccer"){
 				$sql = "SELECT 1 FROM soccer AS sc JOIN schedule AS s ON sc.schedule_id WHERE schedule_id='$gameID'";
 				?>			
-				<a href="../game/soccer.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				<a href="<?php echo $baseUrl?>game/soccer.php?gameID=<?php echo $gameID?>" class='schedule-game'>
 				+------------------------------+<br>
 				<div class='schedule-container'>
 				<div>|<b><?php echo $formattedName?></b></div><div>.....|</div>
@@ -53,6 +53,7 @@
 				<?php
 			}else if($sport == "softball" or $sport == "jvsoftball" or $sport == "baseball" or $sport == "jvbaseball"){
 				
+				//gets live game stats
 				$sqlbb = "SELECT * FROM live_games AS lg JOIN schedule AS s ON lg.schedule_id = s.id WHERE s.id='$gameID'";
 				$querylg = $db->prepare($sqlbb);
 				$querylg->execute();
@@ -107,7 +108,7 @@
 				<?php
 			}else if($sport == "blax" or $sport == "jvblax"){
 				?>			
-				<a href="./game/blax.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				<a href="<?php echo $baseUrl?>game/blax.php?gameID=<?php echo $gameID?>" class='schedule-game'>
 				+------------------------------+<br>
 				<div class='schedule-container'>
 				<div>|<b><?php echo $formattedName?></b></div><div>.....|</div>
@@ -146,7 +147,7 @@
 				<?php
 			}else if($sport == "glax" or $sport == "jvglax"){
 				?>			
-				<a href="./game/glax.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				<a href="<?php echo $baseUrl?>game/glax.php?gameID=<?php echo $gameID?>" class='schedule-game'>
 				+------------------------------+<br>
 				<div class='schedule-container'>
 				<div>|<b><?php echo $formattedName?></b></div><div>.....|</div>
@@ -185,13 +186,172 @@
 				<?php
 			}else if($sport == "bbball" or $sport == "jvbbball" or $sport == "gbball" or $sport == "jvgbball"){
 				?>
-				<a href="./game/basketball.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				<a href="<?php echo $baseUrl?>game/basketball.php?gameID=<?php echo $gameID?>" class='schedule-game'>
 				+------------------------------+<br>
 				<div class='schedule-container'>
 				<div>|<b><?php echo $formattedName?></b></div><div>.....|</div>
 				</div>
 				<?php
 				$sqlbb = "SELECT * FROM basketball AS bb JOIN schedule AS s ON bb.schedule_id = s.id WHERE s.id='$gameID'";
+				$querybb = $db->prepare($sqlbb);
+				$querybb->execute();
+				$rowbb = $querybb->fetch(PDO::FETCH_ASSOC);
+				if($querybb->rowCount() == 0){
+				?>	<div class='schedule-container'>		
+					<div>|<?php echo $time?> EST </div><div>@<?php echo $home?>|</div>
+					</div>
+					<div class='schedule-container'><div>|<?php echo $home?></div><div>.....|</div></div>
+					<div class='schedule-container'><div>|<?php echo $away?></div><div>.....|</div></div>
+				<?php
+				}else{
+					if($rowbb['completed'] == 1){
+						printf("<div class='schedule-container'><div>|</div><div><b>FINAL</b>|</div></div>");
+						if($rowbb['home_total'] > $rowbb['away_total']){
+							printf("<div class='schedule-container'><div>|<b>%s</div> <div>...%d</b>|</div></div>", $home, $rowbb['home_total']);
+							printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $away, $rowbb['away_total']);
+						}else{
+							printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $home, $rowbb['home_total']);
+							printf("<div class='schedule-container'><div>|<b>%s</div> <div>...%d</b>|</div></div>", $away, $rowbb['away_total']);
+						}
+					}else{
+					//Started not completed, grab current quarter time etc. to replace top line
+					printf("<div class='schedule-container'><div>|<span class='red'>LIVE</span></div> <div>@ %s|</div></div>", $home);
+					printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $home, $rowbb['home_total']);
+					printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $away, $rowbb['away_total']);
+					}
+				}
+				?>
+				+------------------------------+</a><br><br>
+				<?php
+				
+			//FALL SPORTS
+			}else if($sport == "football" or $sport == "jvfootball"){
+							
+				//gets live game stats
+				$sqlbb = "SELECT * FROM live_games AS lg JOIN schedule AS s ON lg.schedule_id = s.id WHERE s.id='$gameID'";
+				$querylg = $db->prepare($sqlbb);
+				$querylg->execute();
+				$livegame = $querylg->fetch(PDO::FETCH_ASSOC);
+				$side = "Own";
+				$yard_line = 50;
+				?>			
+				<a href="<?php echo $baseUrl?>game/football.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				+------------------------------+<br>
+				<div class='schedule-container'>
+				<div>|<b><?php echo $formattedName?></b></div>
+				<?php if($querylg->rowCount() == 0){ //If live game doesn't exist
+					printf("<div>.....|</div>");
+				 }else{ 
+					printf("<div> Q%s %s|</div>", $livegame['period'], $livegame['game_time']);
+					$side = $livegame['info_4'];
+					$yard_line = $livegame['info_5'];
+				}
+				?>
+				</div>
+				<?php
+				$sqlbb = "SELECT * FROM football AS fb JOIN schedule AS s ON fb.schedule_id = s.id WHERE s.id='$gameID'";
+				$querybb = $db->prepare($sqlbb);
+				$querybb->execute();
+				$rowbb = $querybb->fetch(PDO::FETCH_ASSOC);
+				if($querybb->rowCount() == 0){
+				?>	<div class='schedule-container'>		
+					<div>|<?php echo $time?> EST </div><div>@<?php echo $home?>|</div>
+					</div>
+					<div class='schedule-container'><div>|<?php echo $home?></div><div>.....|</div></div>
+					<div class='schedule-container'><div>|<?php echo $away?></div><div>.....|</div></div>
+				<?php
+				}else{
+					if($rowbb['completed'] == 1){
+						printf("<div class='schedule-container'><div>|</div><div><b>FINAL</b>|</div></div>");
+						if($rowbb['home_total'] > $rowbb['away_total']){
+							printf("<div class='schedule-container'><div>|<b>%s</div> <div>...%d</b>|</div></div>", $home, $rowbb['home_total']);
+							printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $away, $rowbb['away_total']);
+						}else{
+							printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $home, $rowbb['home_total']);
+							printf("<div class='schedule-container'><div>|<b>%s</div> <div>...%d</b>|</div></div>", $away, $rowbb['away_total']);
+						}
+					}else{
+					//Started not completed, grab current quarter time etc. to replace top line
+					printf("<div class='schedule-container'><div>|<span class='red'>LIVE</span></div> <div>%s %s|</div></div>", $side, $yard_line);
+					printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $home, $rowbb['home_total']);
+					printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $away, $rowbb['away_total']);
+					}
+				}
+				?>
+				+------------------------------+</a><br><br>
+				<?php
+			}else if($sport == "vball" or $sport == "jvvball"){
+				//gets live game stats
+				$sqlbb = "SELECT * FROM live_games AS lg JOIN schedule AS s ON lg.schedule_id = s.id WHERE s.id='$gameID'";
+				$querylg = $db->prepare($sqlbb);
+				$querylg->execute();
+				$livegame = $querylg->fetch(PDO::FETCH_ASSOC);
+				$serve = ".";
+				?>			
+				<a href="<?php echo $baseUrl?>game/vball.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				+------------------------------+<br>
+				<div class='schedule-container'>
+				<div>|<b><?php echo $formattedName?></b></div>
+				<?php if($querylg->rowCount() == 0){ //If live game doesn't exist
+					printf("<div>.....|</div>");
+				 }else{ 
+					printf("<div> Set %s|</div>", $livegame['period']);
+					$serve = $livegame['info_1'];
+				}
+				?>			
+				</div>
+				<?php
+				$sqlbb = "SELECT * FROM volleyball AS vb JOIN schedule AS s ON vb.schedule_id = s.id WHERE s.id='$gameID'";
+				$querybb = $db->prepare($sqlbb);
+				$querybb->execute();
+				$rowbb = $querybb->fetch(PDO::FETCH_ASSOC);
+				if($querybb->rowCount() == 0){
+				?>	<div class='schedule-container'>		
+					<div>|<?php echo $time?> EST </div><div>@<?php echo $home?>|</div>
+					</div>
+					<div class='schedule-container'><div>|<?php echo $home?></div><div>.....|</div></div>
+					<div class='schedule-container'><div>|<?php echo $away?></div><div>.....|</div></div>
+				<?php
+				}else{
+					if($rowbb['completed'] == 1){
+						printf("<div class='schedule-container'><div>|</div><div><b>FINAL</b>|</div></div>");
+						if($rowbb['home_total'] > $rowbb['away_total']){
+							printf("<div class='schedule-container'><div>|<b>%s</div> <div>...%d</b>|</div></div>", $home, $rowbb['home_total']);
+							printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $away, $rowbb['away_total']);
+						}else{
+							printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $home, $rowbb['home_total']);
+							printf("<div class='schedule-container'><div>|<b>%s</div> <div>...%d</b>|</div></div>", $away, $rowbb['away_total']);
+						}
+					}else{
+					//Started not completed, grab current quarter time etc. to replace top line
+					printf("<div class='schedule-container'><div>|<span class='red'>LIVE</span></div> <div>Serve: %s|</div></div>", $serve);
+					printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $home, $rowbb['home_total']);
+					printf("<div class='schedule-container'><div>|%s</div> <div>...%d|</div></div>", $away, $rowbb['away_total']);
+					}
+				}
+				?>
+				+------------------------------+</a><br><br>
+				<?php
+			}else if($sport == "fhockey" or $sport == "jvfhockey"){
+				//gets live game stats
+				$sqlbb = "SELECT * FROM live_games AS lg JOIN schedule AS s ON lg.schedule_id = s.id WHERE s.id='$gameID'";
+				$querylg = $db->prepare($sqlbb);
+				$querylg->execute();
+				$livegame = $querylg->fetch(PDO::FETCH_ASSOC);
+				?>			
+				<a href="<?php echo $baseUrl?>game/fhockey.php?gameID=<?php echo $gameID?>" class='schedule-game'>
+				+------------------------------+<br>
+				<div class='schedule-container'>
+				<div>|<b><?php echo $formattedName?></b></div>
+				<?php if($querylg->rowCount() == 0){ //If live game doesn't exist
+					printf("<div>.....|</div>");
+				 }else{ 
+					printf("<div> Q%s %s|</div>", $livegame['period'], $livegame['game_time']);
+				}
+				?>	
+				</div>
+				<?php
+				$sqlbb = "SELECT * FROM field_hockey AS fh JOIN schedule AS s ON fh.schedule_id = s.id WHERE s.id='$gameID'";
 				$querybb = $db->prepare($sqlbb);
 				$querybb->execute();
 				$rowbb = $querybb->fetch(PDO::FETCH_ASSOC);
