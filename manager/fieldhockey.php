ec403b59b233a05e7741733fb0c3cc46b8960aee
kevinatoranator <kevguy.vet@gmail.com>
2024-09-01 17:32:56 -0400

    Add files via upload
    Include file

---
+++ manager/fieldhockey.php
@@ -0,0 +1,19 @@
+<?php
+	
+	$actionList = [
+	'<option value="Goal scored by ">Goal scored by </option>',
+	'<option value="Assist by ">Assist by </option>',
+	'<option value="Penalty corner by ">Penalty corner by </option>',
+	'<option value="Save by ">Save by </option>',
+	'<option value="Shot by ">Shot by </option>',
+	'<option value="Shot on goal by ">Shot by </option>',
+	'<option value="Penalty on ">Penalty on </option>',
+	'<option value="Green card on ">Green card on </option>',
+	'<option value="Yellow card on ">Yellow card on </option>'];
+	
+	//live game vars
+	$game_time = "12:00";
+	$goalie = "";
+	
+ 
+	?>
