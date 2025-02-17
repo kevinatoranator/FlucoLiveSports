79af883d14eea8246e56e4d45f0ae331aed38eb0
kevinatoranator <kevguy.vet@gmail.com>
2025-02-17 17:56:08 -0500

    New
    Follows new sport manager structure

---
+++ manager/basketball.php
@@ -0,0 +1,36 @@
+<?php
+	
+	//MOST SPORTS (Not bat or vball)
+	//$minutes = 0;//currently selected minute
+	//$seconds = 0;//currently selected second
+	//$maximumMinutes = 99;//Maximum minutes for selection
+	
+	//$quarter = 1;
+	
+	//Basketball
+	$homeTimeOuts = 5;
+	$awayTimeOuts = 5;
+	$homeFouls = 0;
+	$awayFouls = 0;
+	$actionList = [
+	'<option value="Jumper by ">Jumper by </option>',
+	'<option value="Jumper missed by ">Jumper missed by </option>',
+	'<option value="Layup by ">Layup by </option>',
+	'<option value="Layup missed by ">Layup missed by </option>',
+	'<option value="Dunk by ">Dunk by </option>',
+	'<option value="Dunk missed by ">Dunk missed by </option>',
+	'<option value="3 Pointer by ">3 Pointer by </option>',
+	'<option value="3 Pointer missed by ">3 Pointer missed by </option>',
+	'<option value="Free throw by ">Free throw by </option>',
+	'<option value="Free throw missed by ">Free throw missed by </option>',
+	'<option value="Assist by ">Assist by </option>',
+	'<option value="Block by ">Block by </option>',
+	'<option value="Foul on ">Foul on </option>',
+	'<option value="Turnover by ">Turnover by </option>',
+	'<option value="Steal by ">Steal by </option>',
+	'<option value="Defensive rebound by ">Defensive rebound by </option>',
+	'<option value="Offensive rebound by ">Offensive rebound by </option>',
+	'<option value="Timeout">Timeout</option>',
+	'<option value="Jump ball">Jump ball</option>'];
+	
+?>
