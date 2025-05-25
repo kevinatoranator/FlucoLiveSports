55d2102fcd12dd4f3bf3e1965cfab85fc46ef029
kevinatoranator <kevguy.vet@gmail.com>
2025-05-25 10:23:17 -0400

    Adjustments

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="stylesheet.css">
 </head>
+
 <body>
 <?php 
 $date = date("Y-m-d", strtotime("today" ));
@@ -18,14 +19,10 @@ $affix = '';?>
 <!--Schedule Header-->
 
     <br>
-    <div class="flex justify-between">
-        <a href ="./teams/index.php">Teams</a>
-        <a href ="../../index.php">Schedule</a>
-    </div>
-    <br>
-    <div class="flex justify-between">
-        <a href ="./standings/index.php">Standings</a><a href ="./schedule/district/index.php">District Schedule</a>
-    </div>
+    <?php 
+	include './include/database.php';
+	include './include/header.php';
+	?>
 
 <br>
 <div class="flex justify-between">
@@ -37,16 +34,10 @@ $affix = '';?>
 <!--Schedule Body-->
 
 <?php
-	include './include/database.php';
+	
 
-	$sql = "SELECT s.id AS gameID, s.time, s.game_date, h.short_name AS home, a.short_name AS away, s.location, s.home_id, s.away_id, s.team_id, t.formattedName, t.urlName AS sport FROM $schedule AS s JOIN roster_schools a ON s.away_id=a.id JOIN roster_schools h ON s.home_id=h.id JOIN roster_teams AS t ON s.team_id=t.id";
-
-    try {
-      $db = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
-    } catch (PDOException $e) {
-      echo "Error!:" . $e->getMessage() . "<br/>";
-      die();
-    }
+	$sql = "SELECT s.id AS gameID, s.time, s.game_date, h.short_name AS home, a.short_name AS away, s.location AS location, s.home_id, s.away_id, s.team_id, s.notes AS notes, t.formattedName, t.urlName AS sport FROM $schedule AS s JOIN roster_schools a ON s.away_id=a.id JOIN roster_schools h ON s.home_id=h.id JOIN roster_teams AS t ON s.team_id=t.id WHERE a.short_name='FLUV' OR h.short_name='FLUV'
+	ORDER BY s.time";
 	
 	$query = $db->prepare($sql);
 	$query->execute();
